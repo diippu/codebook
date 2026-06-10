eb108890bf473d759db2356cd08f9ed1a223a904
Dipu Debnath <dipudeb883@gmail.com>
2026-06-11 01:20:12 +0600

    added regular polygon formulas

--- formulas.cpp
+++ formulas.cpp
@@ -11,6 +11,26 @@ Convex polygon point inside:
 For all edges (Pi, Pi+1):
 cross(Pi+1-Pi, X-Pi) >= 0 (or <=0 consistently)
 */
+/* ---------- REGULAR POLYGON ----------
+n = number of sides, s = side length
+PI = acos(-1.0)
+
+Angles (in radians):
+Sum of interior  = (n - 2) * PI
+Single interior  = (n - 2) * PI / n
+Central / Exterior = 2 * PI / n
+
+Apothem (a) [Inradius - center to midpoint of side]:
+a = s / (2 * tan(PI / n))
+
+Circumradius (R) [Center to vertex]:
+R = s / (2 * sin(PI / n))
+
+Area formulas:
+1. From side:  Area = (n * s * s) / (4 * tan(PI / n))
+2. From R:     Area = 0.5 * n * R * R * sin(2 * PI / n)
+3. From Apo:   Area = n * s * a * 0.5
+*/
 /* ---------- TRIANGLE ----------
 Area using cross:
 Area = |cross(B-A, C-A)| / 2
